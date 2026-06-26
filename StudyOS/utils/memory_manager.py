2f7afc742ed119372bf3471d87cf5feda4f82de4
ashritha30 <polamadaashritha@gmail.com>
2026-06-26 22:08:07 +0530

    Fix memory path for Streamlit

--- StudyOS/utils/memory_manager.py
+++ StudyOS/utils/memory_manager.py
@@ -1,27 +1,39 @@
 import json
 import os
 
-MEMORY_FILE = "data/memory.json"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+MEMORY_FILE = os.path.join(DATA_DIR, "memory.json")
+
 
 def load_memory():
+
+    os.makedirs(DATA_DIR, exist_ok=True)
+
     if not os.path.exists(MEMORY_FILE):
-        # return {
-        #     "summaries": [],
-        #     "quizzes": [],
-        #     "plans": []
-        # }
-        return {
-    "summaries": [],
-    "quizzes": [],
-    "plans": [],
-    "topics": [],
-    "quiz_scores": [],
-    "weak_topics": []
-}
+
+        memory = {
+            "summaries": [],
+            "quizzes": [],
+            "plans": [],
+            "topics": [],
+            "quiz_scores": [],
+            "weak_topics": []
+        }
+
+        with open(MEMORY_FILE, "w") as f:
+            json.dump(memory, f, indent=4)
+
+        return memory
 
     with open(MEMORY_FILE, "r") as f:
         return json.load(f)
 
+
 def save_memory(memory):
+
+    os.makedirs(DATA_DIR, exist_ok=True)
+
     with open(MEMORY_FILE, "w") as f:
         json.dump(memory, f, indent=4)
